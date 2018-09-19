a29c671c19a12fa8adda980063508f4b3e98d2f0
Jen E <jen@wecancodeit.org>
2018-09-19 15:27:04 -0400

    initial commit

---
+++ TravelerFall18.Tests/TravelerTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelerFall18.Tests
+{
+    //This is the Traveler Application
+    //  - take in an eye color and return a beach location
+    //  - take in size of shoe and return what hotel you will stay at
+
+    [TestFixture]
+    class TravelerTests
+    {
+        Beach beach = new Beach();
+
+        [Test]
+        public void Enter_Eye_Color_Set_Property()
+        {
+            //Arrange
+            Beach beach = new Beach("blue");
+            //Act
+            string response = beach.EyeColor;
+            //Assert
+            Assert.That(response, Is.EqualTo("blue"));
+        }
+
+        [Test]
+        public void Enter_Blue_Eye_Color_Return_Empty_String()
+        {
+            //Arrange
+            //^ in setup
+            //Act
+            string response = beach.GetBeach("blue");
+            //Assert
+            Assert.That(response, Is.EqualTo(" "));
+        }
+
+        [Test]
+        public void Enter_Green_Eye_Color_Return_Beach()
+        {
+            //Arrange
+            //^ in setup
+            //Act
+            string response = beach.GetBeach("green");
+            //Assert
+            Assert.That(response, Is.EqualTo("Seal Beach"));
+        }
+
+        [Test]
+        public void Enter_Brown_Eye_Color_Return_Beach()
+        {
+            string response = beach.GetBeach("brown");
+            Assert.That(response, Is.EqualTo("Newport Beach"));
+        }
+
+        [Test]
+        public void Enter_Grey_Eye_Color_Return_Beach()
+        {
+            string response = beach.GetBeach("grey");
+            Assert.That(response, Is.EqualTo("Huntington Beach"));
+        }
+
+        [Test]
+        public void Set_Shoe_Size()
+        {
+            beach.ShoeSize = 10.5;
+            double response = beach.ShoeSize;
+            Assert.That(response, Is.EqualTo(10.5));
+        }
+
+        [Test]
+        public void Enter_Shoe_Size_0_Beach_Empty_Return_Hotel_Null()
+        {
+            string location = beach.GetBeach(" ");
+            string response = beach.GetHotel(0, location);
+            Assert.That(response, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void Enter_Shoe_Size_10_And_Seal_Beach_Return_Hotel_Hilton()
+        {
+            string location = beach.GetBeach("green");
+            string response = beach.GetHotel(10, location);
+            Assert.That(response, Is.EqualTo("Hilton"));
+        }
+
+    }
+}
---
+++ TravelerFall18/Beach.cs
@@ -0,0 +1,60 @@
+﻿namespace TravelerFall18
+{
+    public class Beach
+    {
+        //Fields
+        private string eyeColor;
+        private double shoeSize;
+
+        //Properties
+        public string EyeColor
+        {
+            get { return this.eyeColor; }
+            set { this.eyeColor = value; }
+        }
+        public double ShoeSize
+        {
+            get { return this.shoeSize; }
+            set { this.shoeSize = value; }
+        }
+        
+        //Constructors
+        public Beach()
+        {
+
+        }
+        public Beach(string val_eyeColor)
+        {
+            EyeColor = val_eyeColor;
+        }
+        
+        //Methods
+        public string GetBeach(string eyeColor)
+        {
+            if(eyeColor == "green")
+            {
+                return "Seal Beach";
+            }
+            else if(eyeColor == "brown")
+            {
+                return "Newport Beach";
+            }
+            else if(eyeColor == "grey")
+            {
+                return "Huntington Beach";
+            }
+            else
+            {
+                return " ";
+            }
+        }
+        public string GetHotel(double num, string value)
+        {
+            if (num == 10 && value == "Seal Beach")
+            {
+                return "Hilton";
+            }
+            return (null);
+        }
+    }
+}
